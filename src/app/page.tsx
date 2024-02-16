08db5e8074411190b49fe878051ec7bde8852a5e
rahul <rkrahul8181@gmail.com>
2024-02-16 21:56:36 +0530

    add clerk authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { UserButton } from "@clerk/nextjs"
 
 const HomePage = () => {
   return (
     <div>
+     <p >
       Home page
+      </p>
+
+      <UserButton afterSignOutUrl="/" />
     
     </div>
   )
