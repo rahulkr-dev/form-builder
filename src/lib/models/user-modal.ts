e9b41563e762a62ba09384a9e1e5c0d1cf1ac363
rahul <rkrahul8181@gmail.com>
2024-02-17 13:00:43 +0530

    update user-modal(add timestamp)

--- src/lib/models/user-modal.ts
+++ src/lib/models/user-modal.ts
@@ -1,43 +1,47 @@
-import { Schema, model, models,Document } from "mongoose";
-
+import { Schema, model, models, Document } from "mongoose";
 
 export interface IUser extends Document {
-   clerkId:string,
-   email:string,
-   username:string,
-   photo:string,
-   firstName:string,
-   lastName:string
-  }
+  clerkId: string;
+  email: string;
+  username: string;
+  photo: string;
+  firstName: string;
+  lastName: string;
+}
 
-
-const UserSchema = new Schema<IUser>({
-  clerkId: {
-    type: String,
-    required: true,
-    unique: true,
+const UserSchema = new Schema<IUser>(
+  {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  photo: {
-    type: String,
-    required: true,
-  },
-  firstName: {
-    type: String,
-  },
-  lastName: {
-    type: String,
+  {
+    timestamps: true,
+    versionKey: false,
   }
-});
+);
 
 const User = models?.User || model("User", UserSchema);
 
